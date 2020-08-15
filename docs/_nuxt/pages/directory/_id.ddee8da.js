df7d3d5fa22104831239652326a41c99e5e86f91
Nathan Blaylock <ngblaylock@gmail.com>
2020-08-15 13:23:29 -0600

    add docs to push to master to host on gh-pages

---
+++ docs/_nuxt/pages/directory/_id.ddee8da.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{147:function(t,e,n){"use strict";n.r(e);var r=n(25),component=Object(r.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("div",[e("h1",[this._v("Dynamic Link")]),this._v(" "),e("p",[this._v("Route Name: "+this._s(this.$route.params.id))])])}),[],!1,null,null,null);e.default=component.exports}}]);
